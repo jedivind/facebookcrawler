72197d7bce6ea21799b7674962eac82768d05ecf
Vinay Bharadwaj <vbharadwaj6@gatech.edu>
2013-06-05 00:42:55 -0400

    Client example added

--- org/facebook/crawler/CrawlerClient.java
+++ org/facebook/crawler/CrawlerClient.java
@@ -7,7 +7,7 @@ public class CrawlerClient {
 	 */
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-		FacebookDBParams dbParams = new FacebookDBParams("127.0.0.1" , "root" , "AlphaCanisMajoris", "facebook"
+		FacebookDBParams dbParams = new FacebookDBParams("127.0.0.1" , "root" , "", "facebook"
 									, "jdbc" , "mysql" , "3306");
 		FacebookDatabase DB = new FacebookDatabase( dbParams );
 		String accessToken = "CAACEdEose0cBAJQmX7VwQajxecQ7EcEcqnUuVUZAlw6g1NVM4ZBCPJ3ZCHMahArRtfmqy7kscx6OOhTG0PSGNbihikN8hjufo52t3n6JmJ1gAKeNOqxRNRfoPQqhjD8B0Og72CTftXa4YksSPGF482xub6gIuUZD";
